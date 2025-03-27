b027e51959406fa4411a3f105c93a04a3c117e15
Megadurck <romariodurck@gmail.com>
2025-03-27 12:51:34 -0300

    Attached to branch main

--- escola_arte/settings.py
+++ escola_arte/settings.py
@@ -74,21 +74,13 @@ WSGI_APPLICATION = 'escola_arte.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if os.getenv('DATABASE_URL'):
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')),
+
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME'),
-            'USER': os.getenv('DB_USER'),
-            'PASSWORD': os.getenv('DB_PASSWORD'),
-            'HOST': os.getenv('DB_HOST'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
